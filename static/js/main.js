d807f4e5ad261e8bc0beba81970b68b6f9c39ad7
Pablo Fernandez <fernandezpablo85@gmail.com>
2017-01-09 20:52:55 -0300

    toggle notes, fix links on first episode notes

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
-	console.log("Hello!");
-});
+function go() {
+	$('.show-notes').on('click', function (event) {
+		let anchor = event.target;
+		$(anchor).next('.notes').toggle();
+		$(anchor).toggleClass('shown');
+		// so it doesn't "jump" to the top of the page.
+		event.preventDefault();
+	});
+}
+
+console.warn('WARNING: you should follow us on twitter: @sinhumopodcast :)')
+$(document).ready(go);
